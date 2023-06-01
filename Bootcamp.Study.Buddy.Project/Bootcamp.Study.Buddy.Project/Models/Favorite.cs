1c77db3973d7f5e6b4a0a4e9ea054ce73532d981
Asma Razaq <asmarazaq@rocketmortgage.com>
2023-06-01 17:12:50 -0400

    connected ASP.NET to DB and created DB context

---
+++ Bootcamp.Study.Buddy.Project/Bootcamp.Study.Buddy.Project/Models/Favorite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bootcamp.Study.Buddy.Project.Models;
+
+public partial class Favorite
+{
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? QuestionId { get; set; }
+}
---
+++ Bootcamp.Study.Buddy.Project/Bootcamp.Study.Buddy.Project/Models/StudentBootCampDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bootcamp.Study.Buddy.Project.Models;
+
+public class StudentBootCampDbContext : DbContext
+{
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<Favorite> Favorites { get; set; }
+
+    public StudentBootCampDbContext(DbContextOptions<StudentBootCampDbContext> options)
+        : base(options)
+    {
+    }   
+    
+    } 
+
+   
